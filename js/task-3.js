92336b1695620ffbf360f4837b72cc2222436166
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-04-22 22:18:10 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,17 @@
-function checkForSpam(message) {
-    const newMessage  = message.toLowerCase();
-// const saleMessage.toLowerCase = 'SALE';
-    // const spamMessage.toLowerCase = 'SPAM';
-    // const hasSale = newMessage.includes('sale');
-    // const hasSpam = newMessage.includes('spam');
-    const type = newMessage.includes('sale') || newMessage.includes('spam') ? true : false;
-    return type;
-    // return  newMessage.includes('sale') || newMessage.includes('spam');
-//     if (newMessage.includes('sale') || newMessage.includes('spam')) {
-    //    let message = message.toLowerCase;
-//     return true;
-// } else {
-//     return false;
-//     }
+function filterArray(numbers, value) {
+ let array1 = [];
+for (const i of numbers){
+    if (i > value) {
+        // array1 += i;
+        array1.push(i);
+        }
+    }
+    return array1;    
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
