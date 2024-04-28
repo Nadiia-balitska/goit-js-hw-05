50777236f9c72656e36e049c5a9d2fb5b2e8978f
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-04-28 19:14:26 +0300

    finished

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,30 @@
-function filterArray(numbers, value) {
- let array1 = [];
-for (const i of numbers){
-    if (i > value) {
-        // array1 += i;
-        array1.push(i);
-        }
-    }
-    return array1;    
-}
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+    changeUsername(newName) {
+        this.username = newName;
+        // for (const name in this.profile) {
+        // }
+    },
+    updatePlayTime(hours) {
+       this.playTime += hours;
+        //   for (const time in this.profile) {
+        // }
+      
+        
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
